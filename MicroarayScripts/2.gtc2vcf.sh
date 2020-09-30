1a8cac0c5a0504d3b663c9d182a97d1d29f934ec
drew <andrew.s.judell@gmail.com>
2020-09-30 06:51:34 -0400

    added the 3rd step ing the GSA pipeline

--- MicroarayScripts/2.gtc2vcf.sh
+++ MicroarayScripts/2.gtc2vcf.sh
@@ -5,43 +5,43 @@
 # set index dir location env variables etc
 ###########################################################################
 
-export INDEX_DIR=$HOME/Downloads/GoodCell-Resources/GuliosAnalysis/ReferenceGenomes/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set
-export REFDIR=$HOME/Downloads/GoodCell-Resources/GuliosAnalysis/ReferenceGenomes
+export WORK_DIR=/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data
+export INDEX_DIR=/media/drew/easystore/ReferenceGenomes/GCA_000001405.15_GRCh38_no_alt_analysis_set
+export ref=$INDEX_DIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+export REF_DIR=/media/drew/easystore/ReferenceGenomes
 
 for file in $INDEX_DIR; do
     ln -s $file
 done
 
-ref=$INDEX_DIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
-
 declare -A wdir=( ["20180117"]="2018_07" ["20200110"]="2020_01" )
-declare	-A refdir=( ["20180117"]="GSA_24v1_0" ["20200110"]="GSA_24v2_0" )
-declare	-A bpm=( ["20180117"]="GSA-24v1-0_A1.bpm" ["20200110"]="GSA-24v2-0_A1.bpm" ) 
-declare -A egt=( ["20180117"]="GSA-24v1-0_A1_ClusterFile.egt" ["20200110"]="GSA-24v2-0_A1_ClusterFile.egt" )
-declare -A csv=( ["20180117"]="GSA-24v1-0_A1.csv" ["20200110"]="GSA-24v2-0_A1.csv" )
-declare -A sam=( ["20180117"]="" ["20200110"]="" ) #["20200320"]="CCPMBiobankMEGA2_20002558X345183_A1.bam" )
-#declare -A opts=( ["20180117"]="" ["20200110"]="-s ^8033684100" )
-#["20200320"]="CCPMBiobank_MEGA2_A1.bpm" )
-
-for pfx in 20180117; do
-    wdir=${wdir[$pfx]}
-    refdir=${refdir[$pfx]}
-    bpm=${bpm[$pfx]}
-    egt=${egt[$pfx]}
-    csv=${csv[$pfx]}
-    sam=${sam[$pfx]}
-done
-
-touch $HOME/Downloads/GoodCell-Resources/GuliosAnalysis/$wdir/$pfx.index.GRCh38.bcf
-ln -s $HOME/Downloads/GoodCell-Resources/GuliosAnalysis/$wdir/$pfx.index.GRCh38.bcf
-export TMP_DIR=$HOME/Downloads/GoodCell-Resources/GuliosAnalysis/$wdir/bcftools-sort.XXXXXX
+declare -A rfdir=( ["20180117"]="GSA_24v1_0" ["20200110"]="GSA_24v2_0" )
+declare -A bpm=( ["20180117"]="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
+declare -A egt=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A1_ClusterFile.egt" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A1_ClusterFile.egt" )
+declare -A csv=( ["20180117"]="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.csv" )
+declare -A sam=( ["20180117"]="" ["20200110"]="" ["20200320"]="CCPMBiobankMEGA2_20002558X345183_A1.bam" )
+declare -A opts=( ["20180117"]="" ["20200110"]="-s ^8033684100" ["20200320"]="CCPMBiobank_MEGA2_A1.bpm" )
 
 ###########################################################################
 # Run gtc2vcf on GTC directory
 ###########################################################################
 
-bcftools +gtc2vcf -Ou -f $ref -b $REFDIR/$refdir/$bpm  -e $REFDIR/$refdir/$egt -x $pfx.sex -g GTCs -o $pfx.GRCh38.bcf | \
+for pfx in 20180117 20200110; do
+    workdir=$WORK_DIR/${wdir[$pfx]}
+    wdr=${wdir[$pfx]}
+    refdir=${rfdir[$pfx]}
+    bpm=${bpm[$pfx]}
+    egt=${egt[$pfx]}
+    csv=${csv[$pfx]}
+    sam=${sam[$pfx]}
+    touch $workdir/$wdr.index.GRCh38.bcf
+    ln -s $workdir/$wdr.index.GRCh38.bcf
+    export TMP_DIR=$workdir/bcftools-sort.XXXXXX
+    bcftools +gtc2vcf --gtcs $workdir/GTCs -o $workdir/$wdr.maps.tsv
+    bcftools +gtc2vcf -Ou -f $ref -b $bpm  -e $egt -x $workdir/$wdr.sex -g $workdir/GTCs -o $workdir/$wdr.GRCh38.bcf | \
     bcftools sort -Ou -T $TMP_DIR | \
-    bcftools reheader -s ../map.tsv | \
-    bcftools norm --no-version -Ou -o $pfx.GRCh38.bcf -c x -f $ref && \
-    bcftools index -f $pfx.index.GRCh38.bcf
+    bcftools reheader -s $workdir/$wdr.map.tsv | \
+    bcftools norm --no-version -Ou -o  $workdir/$wdr.GRCh38.bcf -c x -f $ref && \
+    bcftools index -f  $workdir/$wdr.index.GRCh38.bcf
+    bcftools +gtc2vcf --no-version -c $csv -s $sam -o ${csv%.csv}.GRCh38.csv
+done
