86bfa3efa4d318d9c0e4e12388d65a0b25bd0320
drew <andrew.s.judell@gmail.com>
2020-10-15 16:20:41 -0400

    incremental changes to both pipelines

--- MiSeqScripts/2.MiSeq-remove_dups.sh
+++ MiSeqScripts/2.MiSeq-remove_dups.sh
@@ -7,13 +7,14 @@
 export REFDIR=/media/drew/easystore/ReferenceGenomes/
 export IDX_DIR=$REFDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set
 export REF=$IDX_DIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
-export WORK_DIR=/media/drew/easystore/GoodCell-Resources/GuniosAnalysis/MiSeq_Data
-ln -s $HOME/toolbin/picard.jar
-declare -A miseqdir=( ["2019_09"]="2019_09" ["2019_12"]="2019_12" )
-declare -A datadir=( ["2019_09"]="MiSeq_Results_out" ["2019_12"]="MiSeq_Results_out" )
+export WORK_DIR=/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/MiSeq_Data
+
 for file in $IDX_DIR; do
     ln -s $file
 done
+
+declare -A miseqdir=( ["2019_09"]="2019_09" ["2019_12"]="2019_12" )
+declare -A datadir=( ["2019_09"]="MiSeq_Results_out" ["2019_12"]="MiSeq_Results_out" )
 
 ###########################################################################
 ## REMOVE DUPLICATES                                                     ##
@@ -22,11 +23,13 @@ done
 for pfx in 2019_09 2019_12; do
     miseqdir=${miseqdir[$pfx]}
     datadir=${datadir[$pfx]}
-    cd $miseqdir
-    sm_arr=( $(cat sm.txt) )
+    export WORKDIR=$WORK_DIR/$miseqdir
+    export INPUT_FILE=$WORKDIR/sm.txt
+    cd $WORKDIR
+    sm_arr=( $(cat $INPUT_FILE) )
     n=${#sm_arr[@]}
     for i in $(seq 1 $n); do
-	sm_arr=( $(cat sm.txt) ); \
+	sm_arr=( $(cat $INPUT_FILE) ); \
 	sm=${sm_arr[(($i-1))]}; \
 	java -jar $HOME/toolbin/picard.jar MarkDuplicates I=$datadir/$sm.raw.bam  \
 	    O=$datadir/$sm.tmp.bam M=$datadir/$sm.dups_metrics.txt && \
--- MicroarayScripts/2.gtc2vcf.sh
+++ MicroarayScripts/2.gtc2vcf.sh
@@ -1,6 +1,7 @@
 #!bin/bash
-# CONVERT DATA FROM GTC TO VCF
 
+###########################################################################
+# CONVERT DATA FROM GTC TO VCF
 ###########################################################################
 # set env variables for index locations
 ###########################################################################
@@ -17,35 +18,33 @@ done
 #bcftools +gtc2vcf -c $MEGA_DIR/CCPMBiobankMEGA2_20002558X345183_A1.csv --fasta-flank | \
 #  bwa mem -M $ref - | samtools view -bS -o $MEGA_DIR/CCPMBiobankMEGA2_20002558X345183_A1.bam
 
-
 declare -A wdir=( ["20180117"]="2018_07" ["20200110"]="2020_01" )
-declare -A bpm=( ["20180117"]="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
+declare -A gsa=(  ["20180117"]="GSA-24v1_0"  ["20200110"]="GSA_24v2_0" )
+declare -A bpm=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
 declare -A egt=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A1_ClusterFile.egt" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A1_ClusterFile.egt" )
-declare -A csv=( ["20180117"]="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.csv" )
-declare -A sam=( ["20180117"]="" ["20200110"]="" ["20200320"]="CCPMBiobankMEGA2_20002558X345183_A1.bam" )
-declare -A opts=( ["20180117"]="" ["20200110"]="-s ^8033684100" ["20200320"]="CCPMBiobank_MEGA2_A1.bpm" )
-
+declare -A csv=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.csv" )
+declare -A sams=( ["20180117"]="" ["20200110"]="" ["20200302"]="" ["20200319"]="" ["20200320"]="/media/drew/easystore/ReferenceGenomes/MEGA_8v2_0/CCPMBiobankMEGA2_20002558X345183_A1.bam" )
 ###########################################################################
 # Run gtc2vcf on GTC directory
 ###########################################################################
 
 for pfx in 20180117 20200110; do
-    workdir=$GSA_DIR/${wdir[$pfx]}
-    wdr=${wdir[$pfx]}
+    wdir=${wdir[$pfx]}
+    gsa=${gsa[$pfx]}
     bpm=${bpm[$pfx]}
     egt=${egt[$pfx]}
     csv=${csv[$pfx]}
-    sam=${sam[$pfx]}
-    touch $wdr.GRCh38.bcf
-    touch $wdr.index.GRCh38.bcf
-    if [ -n "$sam" ]; then \
-	bcftools +gtc2vcf -Ou -f $ref -b $bpm -e $egt -c $csv -s $sam --gtcs $workdir/GTCs -x $workdir/$wdr.sex;
-    else \
-	bcftools +gtc2vcf -Ou -f $ref -b $bpm -e $egt -c $csv --gtcs $workdir/GTCs -x $workdir/$wdr.sex; \
+    sam=${sams[$pfx]}
+    cd $GSA_DIR/$wdir
+    pwd
+    if [ -n "$sam" ]; then
+	bcftools +gtc2vcf --no-version -Ou -f $ref -b $bpm -e $egt -c $csv -s $sam --gtcs GTCs -x $gsa.sex;
+    else
+	bcftools +gtc2vcf --no-version -Ou -f $ref -b $bpm -e $egt -c $csv --gtcs GTCs -x $gsa.sex;
     fi | \
-	bcftools sort -Oz -T ./bcftools-sort.XXXXXX | \
-	bcftools reheader -s $workdir/$wdr.map.tsv | \
-	bcftools norm --no-version -Ou -o  $workdir/$wdr.GRCh38.bcf -c x -f $ref && \
-	bcftools index -f  $workdir/$wdr.GRCh38.bcf
-#        bcftools +gtc2vcf --no-version -c $csv -s $sam -o ${csv%.csv}.GRCh38.csv
+	bcftools sort -Ou -T ./bcftools-sort.XXXXXX | \
+	bcftools reheader -s $gsa.map.tsv | \
+	bcftools norm --no-version -Ob -o  $gsa.GRCh38.bcf -c x -f $ref && \
+	bcftools index -f  $gsa.GRCh38.bcf
+        bcftools +gtc2vcf --no-version -c $csv -s $sam -o ${csv%.csv}.GRCh38.csv
 done
