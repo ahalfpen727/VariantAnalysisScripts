8a7418b649a47fa36d06a62d3973868360f7d630
drew <andrew.s.judell@gmail.com>
2020-10-18 23:57:17 -0400

    more edits for reference retreival files

---
+++ Genomics-Reference-Files-and-their-Retreival/All.chrs.GRCh38.genotypes.20170504.bcf.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+####################################################                                               
+# Set env variables and  work directory                                                            ####################################################                                                                                                            
+export REFDIR="/media/drew/easystore/ReferenceGenomes"
+export IDXDIR=$REFDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set/
+export REFFA=$IDXDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+cd $IDXDIR
+
+wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chr{{1..22},X,Y}_GRCh38.genotypes.20170504.vcf.gz{,.tbi}
+
+for chr in {1..22} X Y; do
+  (bcftools view --no-version -h ALL.chr${chr}_GRCh38.genotypes.20170504.vcf.gz | \
+    grep -v "^##contig=<ID=[GNh]" | sed 's/^##contig=<ID=MT/##contig=<ID=chrM/;s/^##contig=<ID=\([0-9XY]\)/##contig=<ID=chr\1/'; \
+  bcftools annotate --no-version -x INFO/END ALL.chr${chr}_GRCh38.genotypes.20170504.vcf.gz | \
+  bcftools view --no-version -H -c 2 | \
+  grep -v "[0-9]|\.\|\.|[0-9]" | sed 's/^/chr/') | \
+  bcftools norm --no-version -Ou -m -any | \
+  bcftools norm --no-version -Ob -o ALL.chr${chr}_GRCh38.genotypes.20170504.bcf \
+	   -d none -f $REFFA && bcftools index -f ALL.chr${chr}_GRCh38.genotypes.20170504.bcf
+done
