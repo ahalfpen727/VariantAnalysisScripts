31df21f71280d29d50dcab0dd77eecdc0280ceb9
ahalfpen727 <andrew.s.judell@gmail.com>
2021-02-03 13:25:23 -0500

    large refinements

--- MicroarayScripts/2.idat2gtc.sh
+++ MicroarayScripts/2.idat2gtc.sh
@@ -4,35 +4,31 @@
 # set env variable and link ref files
 ###########################################################################
 export REFDIR="/media/drew/easystore/ReferenceGenomes"
-export wrkdr="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data"
-cd $wrkdr
-declare -A dirs=( ["20180117"]="2018_07" ["20200110"]="2020_01" )
-declare -A bpms=( ["20180117"]="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
+export wrkdr="/media/drew/easystore/Current-Analysis/AnalysisBaseDir/GSA_Data"
+declare -A dir=( ["20180117"]="2018_07" ["20200110"]="2020_01" )
+declare -A bpms=( ["20180117"]="/media/drew/easystore/Current-Analysis/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
 declare -A egts=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A1_ClusterFile.egt" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A1_ClusterFile.egt" )
-declare -A csvs=( ["20180117"]="/media/drew/easystore/GoodCell-Resources/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.csv" )
+declare -A csvs=( ["20180117"]="/media/drew/easystore/Current-Analysis/AnalysisBaseDir/GSA_Data/2018_07/GenomeStudio_Files/Manifest_Files/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.csv" )
 ##########################################################################
 ## CONVERT GSA DATA FROM IDAT TO GTC                                     ##
 ###########################################################################
+cd $wrkdr
 for pfx in 20180117 20200110; do
-    dir=${dirs[$pfx]}
+    dr=${dir[$pfx]}
     bpm=${bpms[$pfx]}
     egt=${egts[$pfx]}
-    mkdir -p $wrkdr/$dir/GTCs
-    $HOME/toolbin/iaap-cli/iaap-cli gencall $bpm $egt $wrkdr/$dir/GTCs -f $wrkdr/$dir/Raw_Data/ -g
-    bcftools +gtc2vcf --gtcs  $wrkdr/$dir/GTCs -o $wrkdr/$dir/$pfx.gtc.tsv
+    mkdir -P ./$dir/GTCs
+    iaap_gencall $bpm $egt $dr/GTCs -f $dr/Raw_Data/ -g
+    bcftools +gtc2vcf --gtcs  $dr/GTCs -o $dr/$pfx.gtc.tsv
 done
 
 for pfx in 20180117 20200110; do
-    dir=${dirs[$pfx]}
-    touch $wrkdr/$dir/dir.txt
-    find -iname "*_idat" | xargs $wrkdr/$dir/dir.txt
-    for f in $wrkdr/$dir/dir.txt; do
-    dir=$f
-    cd $dir
-    touch $wrkdr/$dir/files.txt
-    find -iname "*.idat" | xargs > $wrkdr/$dir/files.txt
-    for f in $wrkdr/$dir/files.txt; do
+    dr=${dir[$pfx]}
+    cd $dr
+    touch files.txt
+    find -iname "*.idat" | xargs > files.txt
+    for f in files.txt; do
 	bcftools +gtc2vcf -i -g $f
     done
-    done
+    cd ../
 done
--- MicroarayScripts/3.gtc2vcf.sh
+++ MicroarayScripts/3.gtc2vcf.sh
@@ -5,7 +5,7 @@
 ###########################################################################
 export GSADIR=/media/drew/easystore/Current-Analysis/AnalysisBaseDir/GSA_Data
 export MEGADIR=/media/drew/easystore/ReferenceGenomes/MEGA_8v2_0
-export REFDIR=/media/drew/easystore/ReferenceGenomes
+export REFDIR=/media/drew/easystore/ReferenceGenomes/GRCh38
 export INDEXDIR=$REFDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set
 export REFFA=$INDEXDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
 export REFFAI=$INDEXDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fai
@@ -16,10 +16,10 @@ export REFFAI=$INDEXDIR/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fai
 
 declare -A wdir=( ["20180117"]="2018_07" ["20200110"]="2020_01" )
 declare -A gsa=(  ["20180117"]="GSA-24v1_0"  ["20200110"]="GSA_24v2_0" )
-declare -A bpm=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
-declare -A egt=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A1_ClusterFile.egt" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A1_ClusterFile.egt" )
-declare -A csv=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GSA_24v1_0/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GSA_24v2_0/GSA-24v2-0_A2.csv" )
-declare -A sam=( ["20180117"]="" ["20200110"]="" ["20200302"]="" ["20200319"]="" ["20200320"]="/media/drew/easystore/ReferenceGenomes/MEGA_8v2_0/CCPMBiobankMEGA2_20002558X345183_A1.bam" )
+declare -A bpm=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GRCh38/GSA_24v1_0/GSA-24v1-0_A2.bpm" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GRCh38/GSA_24v2_0/GSA-24v2-0_A2.bpm" )
+declare -A egt=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GRCh38/GSA_24v1_0/GSA-24v1-0_A1_ClusterFile.egt" ["20200110"]="/media/drew/easystore/ReferenceGenomes/GRCh38/GSA_24v2_0/GSA-24v2-0_A1_ClusterFile.egt" )
+declare -A csv=( ["20180117"]="/media/drew/easystore/ReferenceGenomes/GRCh38/GSA_24v1_0/GSA-24v1-0_A2.csv"  ["20200110"]="/media/drew/easystore/ReferenceGenomes/GRCh38/GSA_24v2_0/GSA-24v2-0_A2.csv" )
+declare -A sam=( ["20180117"]="" ["20200110"]="" ["20200302"]="" ["20200319"]="" ["20200320"]="/media/drew/easystore/ReferenceGenomes/GRCh38/MEGA_8v2_0/CCPMBiobankMEGA2_20002558X345183_A1.bam" )
 
 ###########################################################################
 # Run gtc2vcf on GTC directory
